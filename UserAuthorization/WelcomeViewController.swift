c4a772d7f372635fb2770792a1542673f76d8da3
Nastyfrisky <stpnkvnst@mail.ru>
2021-12-21 19:39:26 +0300

    Основная логика работы приложения

---
+++ UserAuthorization/WelcomeViewController.swift
@@ -0,0 +1,25 @@
+//
+//  WelcomeViewController.swift
+//  UserAuthorization
+//
+//  Created by Анастасия Ступникова on 21.12.2021.
+//
+
+import UIKit
+
+class WelcomeViewController: UIViewController {
+
+    @IBOutlet var welcomeLabel: UILabel!
+    
+    var welcome: String!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        welcomeLabel.text = welcome
+    }
+
+    @IBAction func onTapLogout() {
+        navigationController?.popViewController(animated: true)
+    }
+}
